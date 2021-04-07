a4b0c5ce81ebe6ec43cf8be70be080973dd84695
Akash Takawale <atakawal@tibco.com>
2021-04-07 16:46:52 +0530

    product card updated

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,13 +1,15 @@
 export class Product {
     id: number;
     name: string;
+    mname: string;
     description: string;
     price: number;
     imageUrl: string;
 
-    constructor(id: number, name: string, description: string, price: number, imagUrl: string){
+    constructor(id: number, name: string, mname: string, description: string, price: number, imagUrl: string){
         this.id = id;
         this.name = name;
+        this.mname = mname;
         this.description = description;
         this.price = price;
         this.imageUrl = imagUrl;
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,12 +8,12 @@ import { Product } from '../models/product';
 export class ProductService {
 
   products: Product[] = [
-    new Product(1, 'Marigold', 'Marigolds in the garden are cheerful annuals that thrive in bright sunlight.', 250, 'assets/img/marigold.jpg'),
-    new Product(2, 'Night Jasmine', 'One of the beautiful plants whose flowers fall on the ground after blooming.', 500, 'assets/img/prajakt.jpg'),
-    new Product(3, 'Mogra', 'The beautiful white flowers have a strong, but pleasant scent that is enduring.', 300, 'assets/img/mogra.jpg'),
-    new Product(4, 'Sonchafa', 'It is known for its fragrant flowers, and its timber used in woodworking', 500, 'assets/img/sonchafa.jpg'),
-    new Product(5, 'Hibiscus', 'Hibiscus plants are known for their large, colorful flowers and medical use', 150, 'assets/img/hibiscus.jpg'),
-    new Product(6, 'Zinnia', 'Zinnia is a genus of plants of the sunflower tribe within the daisy family', 150, 'assets/img/zinnia.jpg'),
+    new Product(1, 'Hibiscus', 'जास्वंद ', 'Hibiscus plants are known for their large, colorful flowers and medical use', 275, 'assets/img/hibiscus.jpg'),
+    new Product(2, 'Night Jasmine', 'पारिजातक', 'One of the beautiful plants whose flowers fall on the ground after blooming.', 500, 'assets/img/prajakt.jpg'),
+    new Product(3, 'Marigold', 'झेंडू', 'Marigolds in the garden are cheerful annuals that thrive in bright sunlight.', 150, 'assets/img/marigold.jpg'),
+    new Product(4, 'Magnolia champaca', 'सोनचाफा', 'It is known for its fragrant flowers, and its timber used in woodworking', 300, 'assets/img/sonchafa.jpg'),
+    new Product(5, 'Zinnia', 'झेंडू ', 'Zinnia is a genus of plants of the sunflower tribe within the daisy family', 450, 'assets/img/zinnia.jpg'),
+    new Product(6, 'Jasmine sambac', 'मोगरा', 'The beautiful white flowers have a strong, but pleasant scent that is enduring.', 225, 'assets/img/mogra.jpg'),
   ];
 
   cartCopy = [];
